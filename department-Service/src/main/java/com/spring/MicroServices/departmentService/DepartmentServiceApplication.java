119642a1b5620fa34d1c126747df3b4f27f7f5cb
PranayXOX <Pranay.dmnsns@gmail.com>
2023-09-11 13:05:22 +0530

    Spring Cloud Eureka Service implemented for Service Registry and Recovery. Department and Employee Services Registerd as Eureka Server Clients.

--- department-Service/src/main/java/com/spring/MicroServices/departmentService/DepartmentServiceApplication.java
+++ department-Service/src/main/java/com/spring/MicroServices/departmentService/DepartmentServiceApplication.java
@@ -1,6 +1,7 @@
 package com.spring.MicroServices.departmentService;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
